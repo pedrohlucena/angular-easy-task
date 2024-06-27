8fa1b4134dc2dbc746dc07e1ebe69660a241519e
Pedro <pedrohlucena413@gmail.com>
2024-06-27 20:16:30 -0300

    add user name on the screen on user card click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+  
+  get selectedUser () {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  };
 
   onSelectUser(id: string) {
-    const user = this.users.find((user) => user.id === id);
-    console.log(user)
+    this.selectedUserId = id;
   }
 }
