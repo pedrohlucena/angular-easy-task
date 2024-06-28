ad4e0164a1abdd18e72c86ee17515fb78b8fdce6
Pedro <pedrohlucena413@gmail.com>
2024-06-27 21:10:35 -0300

    add user type modularization

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+};
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,7 +12,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: { id: string; name: string; avatar: string };
+  @Input({ required: true }) user!: User;
   
   @Output() select = new EventEmitter<string>();
 
