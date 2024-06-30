8907f69a751d003f93798e6d3a1fca58a38d6193
Pedro <pedrohlucena413@gmail.com>
2024-06-30 15:42:45 -0300

    add complete task refactor

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -22,10 +22,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask (id: string) {
-    this.tasksService.removeTask(id);
-  }
-
   onStartAddTask () {
     this.isAddingTask = true;
   }
